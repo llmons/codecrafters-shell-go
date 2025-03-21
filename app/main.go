50ec9cb38923823d07a01780477ac46f170fd8ee
llmons <llmons@foxmail.com>
2025-03-21 13:51:58 +0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -10,17 +10,13 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
-	// bufio.NewReader(os.Stdin).ReadString('\n')
-
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	for{
+		fmt.Fprint(os.Stdout, "$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
